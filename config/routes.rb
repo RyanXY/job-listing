df0fb8f53828caed8aa194305e598df951050654
ryanxueying <xueying.ryan@gmail.com>
2017-03-01 07:27:25 +0100

    implement admin::jobs CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   resources :jobs
 
+  namespace :admin do
+    resources :jobs
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
